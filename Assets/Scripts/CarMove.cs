bdd9adebb653eb44ec732100392f62a8100c5d18
Eduard Bezduhov <eduard.bezduhov@gmail.com>
2021-02-09 20:33:26 +0300

    Create

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform[] _marks;   //точки по яким їде авто
+
+    private IEnumerator _corutine;
+    private Vector3 _targetPosition;
+    private Vector3 _rotateVector;
+    private int _markIndex;
+
+    private void Start()
+    {
+        _targetPosition = new Vector3();
+        _rotateVector = new Vector3();
+        _markIndex = 0;
+        _targetPosition = _marks[_markIndex].position;
+
+        _markIndex++;
+
+        _corutine = Move();
+        StartCoroutine(_corutine);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //якщо прийшов до цільової точки
+        if (other.GetComponent<Mark>() != null)
+        {
+            _targetPosition = _marks[_markIndex].position;
+
+            _markIndex++;
+
+            if (_markIndex >= _marks.Length)
+                _markIndex = 0;
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        while (true)
+        {
+            _rotateVector = _targetPosition;
+            _rotateVector.y = transform.position.y;
+            transform.LookAt(_rotateVector);
+
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] private Text _fpsText;
+
+    private int _fps;
+    private int _iGlobal;
+    private float[] _fpsArr;
+
+    private void Start()
+    {
+        //робимо цільовий fps рівним частоті оновлень монітора 
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+
+        _fpsArr = new float[60];
+        _iGlobal = 0;
+    }
+
+    private void Update()
+    {
+        if (_iGlobal < 60)
+        {
+            _fpsArr[_iGlobal] = (1 / Time.unscaledDeltaTime);
+            _iGlobal++;
+        }
+        else
+        {
+            _fps = (int)(_fpsArr.Sum()) / _fpsArr.Length;
+            _fpsText.text = "fps: " + _fps.ToString();
+            _iGlobal = 0;
+        }
+    }
+}
